63d8da5811f7fac0914d9c8902221b6395bcdca4
Bivek Gharti <gcbibek3353@gmail.com>
2025-01-06 12:00:11 +0530

    auth using clerk implemented

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
@@ -9,6 +10,12 @@ const page = () => {
       <div className='flex w-fit items-center justify-center gap-2'>
         <p className='documet-title'>HardCoded Title</p>
       </div>
+      <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
       </Header>
       <Editor />
     </div>
