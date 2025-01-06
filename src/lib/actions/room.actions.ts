2dea1c53375ff16149731ac2b52d8514060a040a
Bivek Gharti <gcbibek3353@gmail.com>
2025-01-06 13:55:34 +0530

    server action added for create documet:

---
+++ src/lib/actions/room.actions.ts
@@ -0,0 +1,35 @@
+'use server';
+
+import { nanoid } from 'nanoid'
+import { liveblocks } from '../liveblocks';
+import { revalidatePath } from 'next/cache';
+import { getAccessType, parseStringify } from '../utils';
+import { redirect } from 'next/navigation';
+
+export const createDocument = async ({ userId, email }: CreateDocumentParams) => {
+  const roomId = nanoid();  // creating a unique room Id for every room .
+
+  try {
+    const metadata = {
+      creatorId: userId,
+      email,
+      title: 'Untitled'
+    }
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ['room:write']
+    }
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: []
+    });
+    
+    revalidatePath('/');
+
+    return (room);
+  } catch (error) {
+    console.log(`Error happened while creating a room: ${error}`);
+  }
+}
