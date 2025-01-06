de293dd687870099347bbb79697e75f139378234
Bivek Gharti <gcbibek3353@gmail.com>
2025-01-06 11:21:26 +0530

    text-editor and header added

---
+++ src/app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/Header'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Header>
+      <div className='flex w-fit items-center justify-center gap-2'>
+        <p className='documet-title'>HardCoded Title</p>
+      </div>
+      </Header>
+      <Editor />
+    </div>
+  )
+}
+
+export default page
