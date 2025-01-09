57fc04069ae014a9332317c2c8bfe6d2155b73ec
Bivek Gharti <gcbibek3353@gmail.com>
2025-01-09 10:30:16 +0530

    error while getting users permissions and roles

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import CollaborativeRoom from '@/components/CollaborativeRoom'
 import { getDocument } from '@/lib/actions/room.actions'
+import { getClerkUsers } from '@/lib/actions/user.actions'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 
@@ -15,7 +16,19 @@ const page = async({params} : SearchParamProps) => {
     userId : clerkUser.emailAddresses[0].emailAddress,
   })
   if(!room) redirect('/');
-  console.log('room data is ' + room);
+  // console.log('room data is ' + room);
+
+  const userIds = Object.keys(room.usersAccesses);
+  const users = await getClerkUsers({userIds});
+  console.log(users);
+  
+
+  // const usersData = users.map((user:User)=>({
+  //   ...user,
+  //   userType : room.usersAcceeses[user.email]?.includes('room:write') ? 'editor' : 'viewer'
+  // }))
+
+  const currentUserType = room.usersAccesses[clerkUser.emailAddresses[0].emailAddress]?.includes('room:write') ? 'editor' : 'viewer';
 
   // We need to get the users access permissions 
 
@@ -24,6 +37,8 @@ const page = async({params} : SearchParamProps) => {
      <CollaborativeRoom 
      roomId = {id}
      roomMetadata = {room.metadata}
+    //  users = {usersData}
+     currentUserType = {currentUserType}
      />
     </main>
   )
